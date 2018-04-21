530f8e1b5f9ca6c03f1cfaeb33d02b2995920f51
Nicholas Bemiss <nicholast1990@gmail.com>
2018-04-21 12:39:56 -0400

    Two Player Movement
    Player 1 and 2 are moving. Still a few errors in the player.cs file regarding food and the food text. Marked with "todo", can for easy searching.

--- Rougelike/Assets/Scripts/Player.cs
+++ Rougelike/Assets/Scripts/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
 using UnityEngine.UI;
 
     //Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
-    public class Player : MovingObject
+    public class Player : PlayerController
     {
+		public int playerNumber = 1;
         public float restartLevelDelay = 1f;        //Delay time in seconds to restart level.
         public int pointsPerFood = 10;              //Number of points to add to player food points when picking up a food object.
         public int pointsPerSoda = 20;              //Number of points to add to player food points when picking up a soda object.
@@ -33,7 +34,7 @@ using UnityEngine.UI;
             //Get the current food point total stored in GameManager.instance between levels.
             food = GameManager.instance.playerFoodPoints;
 
-            foodText.text = "Food: " + food;
+            //foodText.text = "Food: " + food; //todo get error
             
             //Call the Start function of the MovingObject base class.
             base.Start ();
@@ -53,29 +54,15 @@ using UnityEngine.UI;
             //If it's not the player's turn, exit the function.
             if(!GameManager.instance.playersTurn) return;
             
-            int horizontal = 0;     //Used to store the horizontal move direction.
-            int vertical = 0;       //Used to store the vertical move direction.
-            
-            
-            //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
-            horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
-            
-            //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
-            vertical = (int) (Input.GetAxisRaw ("Vertical"));
-            
-            //Check if moving horizontally, if so set vertical to zero.
-            if(horizontal != 0)
-            {
-                vertical = 0;
-            }
-            
-            //Check if we have a non-zero value for horizontal or vertical
-            if(horizontal != 0 || vertical != 0)
-            {
-                //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
-                //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
-                AttemptMove<Wall> (horizontal, vertical);
-            }
+			if (Input.GetKey (left)) {
+				AttemptMove<Wall> (-1, 0);
+			}else if (Input.GetKey (right)) {
+				AttemptMove<Wall> (1, 0);
+			}else if (Input.GetKey (up)) {
+				AttemptMove<Wall> (0, 1);
+			}else if (Input.GetKey (down)) {
+				AttemptMove<Wall> (0, -1);
+			}          
         }
         
         //AttemptMove overrides the AttemptMove function in the base class MovingObject
@@ -84,7 +71,7 @@ using UnityEngine.UI;
         {
             //Every time player moves, subtract from food points total.
             food--;
-            foodText.text = "Food: " + food;
+            //foodText.text = "Food: " + food;//todo get error
 
             //Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
             base.AttemptMove <T> (xDir, yDir);
@@ -155,7 +142,7 @@ using UnityEngine.UI;
             {
                 //Add pointsPerSoda to players food points total
                 food += pointsPerSoda;
-                foodText.text = "+" + pointsPerSoda + " Food: " + food;
+                foodText.text = "+" + pointsPerSoda + " Food: " + food;//todo get error with p2
                 SoundManager.instance.RandomizeSfx(drinkSound1,drinkSound2);
                 
                 //Disable the soda object the player collided with.
