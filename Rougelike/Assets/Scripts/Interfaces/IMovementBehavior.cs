20d64c552e5fb178180fc11c098787590f15128d
wkwestfa <wkwestfa@svsu.edu>
2018-08-20 21:33:09 -0400

    Revert "Merge branch 'master' of https://github.com/nickt1990/rougelike-game"
    This reverts commit 82f3767b2f08c3d09c61ce36377ab82ff965a5b9, reversing
    changes made to 703975b8c9bb1510b512ec849ed0e07466b2382d.

--- Rougelike/Assets/Scripts/Interfaces/IMovementBehavior.cs
+++ Rougelike/Assets/Scripts/Interfaces/IMovementBehavior.cs
@@ -5,11 +5,6 @@ using System.Collections;
 public interface IMovementBehavior
 {
     void CheckMovement();
-<<<<<<< HEAD
-=======
-
-    //void InitiateMove(KeyCode direction);
->>>>>>> 0e9fcdd1b57ca0e9212c38a4153abbc4effcaf30
 
     IEnumerator SmoothMovement(Vector3 end);
 }
